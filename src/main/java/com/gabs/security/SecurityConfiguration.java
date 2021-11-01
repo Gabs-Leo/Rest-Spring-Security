c5414055e0a261dcc6ace20cfd8adfe57fddcfc9
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-11-01 17:01:05 -0300

    :sparkles: Criando Usuários

--- src/main/java/com/gabs/security/SecurityConfiguration.java
+++ src/main/java/com/gabs/security/SecurityConfiguration.java
@@ -1,14 +1,28 @@
 package com.gabs.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//Basic Auth
@@ -19,6 +33,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		.anyRequest().authenticated()
 		.and()
 		.httpBasic();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails gabs = User.builder()
+			.username("Gabs")
+			.password(passwordEncoder.encode("pass"))
+			.roles("STUDENT") //ROLE_STUDENT
+			.build();
+		
+		UserDetails aylon = User.builder()
+			.username("Syllient")
+			.password(passwordEncoder.encode("fodase"))
+			.roles("ADMIN")
+			.build();
+		
+		return new InMemoryUserDetailsManager(
+			gabs, aylon
+		);
 	}
 	
 }
