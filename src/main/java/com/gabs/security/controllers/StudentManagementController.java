3466493f74a83654979ac62c2e0cd5f05c206086
GabsLeo <gabrieldemoraistaveira@gmail.com>
2021-11-02 13:39:49 -0300

    :wrench: Anotações de Segurança

--- src/main/java/com/gabs/security/controllers/StudentManagementController.java
+++ src/main/java/com/gabs/security/controllers/StudentManagementController.java
@@ -3,6 +3,7 @@ package com.gabs.security.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,22 +24,29 @@ public class StudentManagementController {
 			new Student(3, "Lorrana")
 	);
 	
+	
+	//hasRole('ROLE_'), hasAnyRole('ROLE_'), 
+	//hasAuthority('permission'), hasAnyAuthority('permission')
 	@GetMapping
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMIN_TRAINEE')")
 	public List<Student> getAllStudents(){
 		return students;
 	}
 	
 	@PostMapping
+	@PreAuthorize("hasAuthority('student:write')")
 	public void addNewStudent(@RequestBody Student student) {
 		System.out.println(student);
 	}
 	
 	@DeleteMapping(path = "{studentId}")
+	@PreAuthorize("hasAuthority('student:write')")
 	public void deleteStudent(@PathVariable("studentId") Integer studentId) {
 		System.out.println(studentId);
 	}
 	
 	@PutMapping(path = "{studentId}")
+	@PreAuthorize("hasAuthority('student:write')")
 	public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
 		System.out.println(studentId + " " + student);
 	}
